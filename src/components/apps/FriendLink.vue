0f817bd92a74991ccfb47fdf27e22e8f06c6768e
royalknight56 <2930470134@qq.com>
2023-08-16 20:54:53 +0800

    feat: 友链

--- src/components/apps/FriendLink.vue
+++ src/components/apps/FriendLink.vue
@@ -1,71 +1,86 @@
 <template>
-    <div>
-        <div class="links">
-            <div class="link" v-for="link in FriendLinks" :key="link.name">
-                <span class="link-title">{{link.name}}</span>
-                <a class="link-a" :href="link.url" target="_blank">{{link.url}}</a>
-            </div>
-        </div>
-    </div>
-
+  <div class="links">
+    <!-- <div class="link" v-for="link in FriendLinks" :key="link.name"> -->
+    <!-- <span class="link-title"></span> -->
+    <a
+      class="link"
+      v-for="link in FriendLinks"
+      :key="link.name"
+      :href="link.url"
+      target="_blank"
+      >{{ link.name }}</a
+    >
+  </div>
+  <!-- </div> -->
 </template>
 <script setup lang="ts">
-const FriendLinks = [
-    {
-        name: 'Vtron',
-        url: 'https://github.com/royalknight56/vtron'
+import { onMounted, reactive } from "vue";
+
+const FriendLinks = reactive<any[]>([]);
+onMounted(() => {
+  fetch("https://myim.online:3100/api/friend", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
     },
-    {
-        name: 'Vtron-demo',
-        url: 'https://github.com/royalknight56/vtron-demo'
-    },
-    {
-        name: 'Vtron-md',
-        url: 'https://github.com/royalknight56/vtron-md'
-    },
-    // {
-    //     name: 'Vtron-store',
-    //     url: 'https://github.com/royalknight56/vtron-store'
-    // },
-    {
-        name: 'Vtron-template',
-        url: 'https://github.com/royalknight56/vtron-template'
-    },
-    {
-        name: 'tol.vip',
-        url: 'https://tol.vip/'
-    }
-]
+    body: JSON.stringify({
+      id: 1,
+    }),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.code === 200) {
+        let tempArray: any[] = [];
+        res.data.forEach((item: any) => {
+          tempArray.push({
+            name: item.title,
+            url: item.content,
+          });
+        });
+        FriendLinks.splice(0, FriendLinks.length, ...tempArray);
+      }
+    });
+});
 </script>
 <style scoped>
-.links{
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-around;
+.links {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: flex-start;
+  justify-items: flex-start;
+  align-content: center;
+  overflow: auto;
+  height: 100%;
 }
-.link{
-    width: 200px;
-    height: 100px;
-    margin: 10px;
-    padding: 10px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    overflow: hidden;
+.link {
+  /* width: 300px; */
+  /* height: 60px; */
+  font-weight: 600;
+  text-decoration: none;
+  color: #3e18b9;
+  margin: 10px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  overflow: hidden;
 }
-.link:hover{
-    border: 1px solid #000;
+.link:hover {
+  border: 1px solid #000;
 }
-.link-title{
-    font-size: 20px;
-    font-weight: bold;
-    text-align: center;
+/* .link-title {
+  font-size: 20px;
+  font-weight: bold;
+  text-align: center;
+  flex-shrink: 0;
 }
-.link-a{
-    font-size: 16px;
-    text-align: center;
-    word-break: break-all;
-}
+.link-a {
+  font-size: 12px;
+  text-align: center;
+  word-break: break-all;
+  flex-shrink: 0;
+} */
 </style>
